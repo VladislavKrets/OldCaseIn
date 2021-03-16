c93c6cdc597d762deefff0e70d1b075f79e6899e
Vladislav Krets <vladislav.krets@gmail.com>
2021-03-16 14:21:23 +0300

    added background

---
+++ frontend/src/components/LoginButtons/LoginButtons.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import {Button} from "react-bootstrap";
+import AuthLoginButton from "../../elements/AuthLoginButton";
+
+class LoginButtons extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: window.innerWidth,
+            height: window.innerHeight,
+            right: 16,
+            top: 50,
+        }
+    }
+
+    openRegistrationPanel = () => {
+        this.props.changePanel("code_registration")
+    }
+
+    openLoginPanel = () => {
+        this.props.changePanel("login")
+    }
+
+    updateSize = () => {
+        this.setState({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        })
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.updateSize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateSize);
+    }
+
+    render() {
+        const right = this.state.height >= 884 ? 16 : 16 - 884 / this.state.height * 1.9;
+        return <div style={{position: "fixed",
+            right: `${right}%`,
+            top: "50%",
+            transform: `translateY(-50%) translateX(-${right}%)`
+        }}>
+                <div style={{paddingBottom: '20px'}}>
+                    <AuthLoginButton onClick={this.openRegistrationPanel}>
+                        Регистрация
+                    </AuthLoginButton>
+                </div>
+                <div>
+                    <AuthLoginButton onClick={this.openLoginPanel}>
+                        Вход
+                    </AuthLoginButton>
+                </div>
+            </div>
+    }
+}
+
+export default LoginButtons
--- frontend/src/panels/Auth.js
+++ frontend/src/panels/Auth.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Redirect, withRouter} from "react-router";
-import LoginButtons from "../components/LoginButtons";
+import LoginButtons from "../components/LoginButtons/LoginButtons";
 import CodeRegistration from "../components/CodeRegistration";
 import Login from "../components/Login";
 import Registration from "../components/Registration";
+import basicAuthImage from '../assets/Pervaya.png'
+import otherAuthImage from '../assets/Formy.png'
 
 class Auth extends React.Component {
     constructor(props) {
@@ -27,7 +29,16 @@ class Auth extends React.Component {
     }
 
     render() {
-        return <div>
+        return <div style={{
+            height: '100vh',
+            boxSizing: 'border-box',
+            backgroundImage: `url(${basicAuthImage})`,
+            backgroundSize: 'contain',
+            objectFit: "fill",
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'right',
+            backgroundColor: '#354488',
+        }}>
             {
                 this.state.panel === 'login_buttons' ?
                     <LoginButtons changePanel={this.changePanel}/>
@@ -37,7 +48,7 @@ class Auth extends React.Component {
                         changePanel={this.changePanel}
                         checkRegistrationCode={this.props.checkRegistrationCode}
                     />
-                        : this.state.panel === 'login' ?
+                    : this.state.panel === 'login' ?
                         <Login login={this.props.login} setToken={this.props.setToken}/>
                         : <Registration register={this.props.register}
                                         registrationCode={this.state.registrationCode}
