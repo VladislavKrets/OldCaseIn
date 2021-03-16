36558cfc7b36c221ebfdb4d546814c70071fb0d5
Viktoria Mirzoyan <mirzoyanvictoria@yandex.ru>
2021-03-16 20:54:36 +0300

    reg component

--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -72,8 +72,8 @@ class Registration extends React.Component {
 
 
                     <Form.Group controlId="formBasicUserLastName">
-                        <Form.Label>Фамилия</Form.Label>
-                        <Form.Control placeholder="Введите хуй" name={'surname'}
+                        <Form.Label>Ф</Form.Label>
+                        <Form.Control placeholder="Введите фамилию" name={'surname'}
                                       value={this.state.regData.surname} onChange={this.handleChange}/>
                     </Form.Group>
 
