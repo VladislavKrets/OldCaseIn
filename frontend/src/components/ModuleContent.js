977871958b19179ef86612082a5301f8365ce4a7
Vladislav Krets <vladislav.krets@gmail.com>
2021-03-17 01:11:55 +0300

    fixed save test bugs

--- frontend/src/components/ModuleContent.js
+++ frontend/src/components/ModuleContent.js
@@ -1,65 +1,92 @@
 import React from 'react'
 import './ModuleContent.css'
-import { Form, Jumbotron } from 'react-bootstrap'
+import {Form, Jumbotron} from 'react-bootstrap'
 
 class ModuleContent extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-      }
-  }
-  handleChange = (event, type) => {
-    if (type === 'radio'){
-        this.props.saveAnswer(event.target.value, {})
-    }
-    else if (type === 'checkbox'){
-        if (event.target.checked) {
-            this.props.saveAnswer(event.target.value, {})
-        }
-        else {
-            this.props.removeAnswer(event.target.value)
+    constructor(props) {
+        super(props);
+        this.state = {
+            isDataLoaded: false,
         }
     }
-    else if (type === 'text') {
-        this.props.saveAnswer(event.target.name, {user_text: event.target.value})
-    }
-  }
 
-  render () {
-    return <div style={{
-      height: '100vh',
-      width: '100%',
-      display: 'flex',
-      alignItems: 'center',
-      boxSizing: 'border-box',
-      background: 'linear-gradient(to bottom, rgb(54, 69, 136) 0%, rgb(111, 118, 181) 40%, ' +
+    handleChange = (event, type) => {
+        if (type === 'radio') {
+            for (let i = 0; i < event.target.form.length; i++) {
+                this.setState({[event.target.form[i].value]: false})
+            }
+            this.setState({
+                [event.target.value]: event.target.checked
+            })
+            this.props.saveAnswer(event.target.value, {})
+        } else if (type === 'checkbox') {
+            this.setState({
+                [event.target.value]: event.target.checked
+            })
+            if (event.target.checked) {
+                this.props.saveAnswer(event.target.value, {})
+            } else {
+                this.props.removeAnswer(event.target.value)
+            }
+        } else if (type === 'text') {
+            this.setState({
+                [event.target.name]: event.target.value
+            })
+            this.props.saveAnswer(event.target.name, {user_text: event.target.value})
+        }
+    }
+
+    render() {
+        const data = {}
+        if (this.props.questionData && !this.state.isDataLoaded) {
+            for (let question of this.props.questionData) {
+                question.answers.forEach(answer => {
+                    if (answer.saved_answer && question.question_type === "text") {
+                        data[`${answer.id}`] = answer.saved_answer.user_text
+                    } else if(question.question_type !== "text") data[`${answer.id}`] = !!answer.saved_answer
+                })
+            }
+            this.setState({
+                ...data,
+                isDataLoaded: true
+            })
+        }
+        return <div style={{
+            height: '100vh',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            boxSizing: 'border-box',
+            background: 'linear-gradient(to bottom, rgb(54, 69, 136) 0%, rgb(111, 118, 181) 40%, ' +
                 'rgb(160, 171, 210) 60%, rgb(218, 216, 234) 80%, rgb(254, 254, 255) 100%)'
-    }}>
+        }}>
             <div style={{
-              width: '100%',
-              margin: '15px',
-              boxSizing: 'border-box',
-              height: '75%',
-              borderRadius: '12px',
-              backgroundColor: 'white'
+                width: '100%',
+                margin: '15px',
+                boxSizing: 'border-box',
+                height: '75%',
+                borderRadius: '12px',
+                backgroundColor: 'white'
             }}>
                 <div style={{
-                  width: '100%',
-                  boxSizing: 'border-box',
-                  height: '100%',
-                  overflowY: 'scroll',
-                  borderRadius: '12px',
-                  backgroundColor: 'white'
+                    width: '100%',
+                    boxSizing: 'border-box',
+                    height: '100%',
+                    overflowY: 'scroll',
+                    borderRadius: '12px',
+                    backgroundColor: 'white'
                 }} className={'moduleContent'}>
                     {
-                        this.props.lessonData && <div style={{ padding: '12px' }}>
+                        this.props.lessonData && <div style={{padding: '12px'}}>
                             <Jumbotron>
-                                <h2 style={{ textAlign: 'center',
-                                    paddingBottom: '10px' }}>Модуль {this.props.currentModule.name}
+                                <h2 style={{
+                                    textAlign: 'center',
+                                    paddingBottom: '10px'
+                                }}>Модуль {this.props.currentModule.name}
                                 </h2>
                                 <h2 style={{
-                                  textAlign: 'center',
-                                  paddingBottom: '10px'
+                                    textAlign: 'center',
+                                    paddingBottom: '10px'
                                 }}>Урок {this.props.lessonData.number}
                                 </h2>
                                 <p>
@@ -69,34 +96,38 @@ class ModuleContent extends React.Component {
                                 </p>
                             </Jumbotron>
                             <Jumbotron>
-                                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                <div style={{display: 'flex', justifyContent: 'center'}}>
                                     <video width={'400px'} controls="controls" src={this.props.lessonData.video}>
                                     </video>
                                 </div>
                             </Jumbotron>
                             {
                                 this.props.questionData && this.props.questionData.map(item => {
-                                  return <Jumbotron>
+
+                                    return <Jumbotron>
                                         <Form>
-                                            <div style={{ fontWeight: 'bold', paddingBottom: '15px'}}>{item.question}</div>
+                                            <div style={{
+                                                fontWeight: 'bold',
+                                                paddingBottom: '15px'
+                                            }}>{item.question}</div>
                                             {
                                                 item.answers.map(answer => {
-                                                  {
-                                                    return <Form.Group controlId={`${item.id}`}>
+                                                    {
+                                                        return <Form.Group controlId={`${item.id}`}>
                                                             <div>
                                                                 {
                                                                     item.question_type === 'radio'
-                                                                      ? <Form.Check
+                                                                        ? <Form.Check
                                                                             type={'radio'}
-                                                                            checked={!!answer.saved_answer}
+                                                                            checked={this.state[`${answer.id}`]}
                                                                             label={answer.answer}
                                                                             value={`${answer.id}`}
                                                                             name={`${item.id}`}
                                                                             onChange={(e) => this.handleChange(e, 'radio')}
                                                                         />
-                                                                      : item.question_type === 'checkbox'
+                                                                        : item.question_type === 'checkbox'
                                                                         ? <Form.Check
-                                                                            checked={!!answer.saved_answer}
+                                                                            checked={this.state[`${answer.id}`]}
                                                                             label={answer.answer}
                                                                             type={'checkbox'}
                                                                             value={`${answer.id}`}
@@ -104,17 +135,17 @@ class ModuleContent extends React.Component {
                                                                             onChange={(e) => this.handleChange(e, 'checkbox')}
                                                                         />
                                                                         : item.question_type === 'text'
-                                                                          ? <Form.Control
+                                                                            ? <Form.Control
                                                                                 onChange={(e) => this.handleChange(e, 'text')}
                                                                                 placeholder="Введите ответ"
-                                                                                name={`${item.id}`}
-                                                                                value={answer.saved_answer ? answer.saved_answer.user_text : null}
+                                                                                name={`${answer.id}`}
+                                                                                value={this.state[`${answer.id}`]}
                                                                                 required="required"/>
-                                                                          : null
+                                                                            : null
                                                                 }
                                                             </div>
                                                         </Form.Group>
-                                                  }
+                                                    }
                                                 })
                                             }
                                         </Form>
@@ -126,7 +157,7 @@ class ModuleContent extends React.Component {
                 </div>
             </div>
         </div>
-  }
+    }
 }
 
 export default ModuleContent
