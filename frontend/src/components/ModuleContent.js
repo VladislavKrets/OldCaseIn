c9d38f2a9d0cebca3950cb32f28c9108a10e61a6
Vladislav Krets <vladislav.krets@gmail.com>
2021-03-16 19:47:23 +0300

    main module changes

--- frontend/src/components/ModuleContent.js
+++ frontend/src/components/ModuleContent.js
@@ -13,7 +13,8 @@ class ModuleContent extends React.Component {
             background: 'linear-gradient(to bottom, rgb(54, 69, 136) 0%, rgb(111, 118, 181) 40%, ' +
                 'rgb(160, 171, 210) 60%, rgb(218, 216, 234) 80%, rgb(254, 254, 255) 100%)'
         }}>
-            <div style={{width: '100%',
+            <div style={{
+                width: '100%',
                 margin: '15px',
                 boxSizing: 'border-box',
                 height: '75%',
@@ -22,14 +23,31 @@ class ModuleContent extends React.Component {
                 backgroundColor: 'white'
             }} className={'moduleContent'}>
                 {
-                    this.props.lessonData && <>
+                    this.props.lessonData && <div style={{padding: '12px'}}>
                         <Jumbotron>
-
+                            <h2 style={{textAlign: 'center', paddingBottom: '10px'}}>Модуль {this.props.modules
+                                .filter(x => x.id === this.props.lessonData.module)[0].name}</h2>
+                            <h2 style={{
+                                textAlign: 'center',
+                                paddingBottom: '10px'
+                            }}>Урок {this.props.lessonData.number}</h2>
+                            <p>
+                                <pre>
+                                {this.props.lessonData.themes}
+                                </pre>
+                            </p>
                         </Jumbotron>
-                    </>
+                        <Jumbotron>
+                            <div style={{display: 'flex', justifyContent: 'center'}}>
+                                <video width={'400px'} controls="controls" src={this.props.lessonData.video}>
+                                </video>
+                            </div>
+                        </Jumbotron>
+                    </div>
                 }
             </div>
         </div>
     }
 }
+
 export default ModuleContent
