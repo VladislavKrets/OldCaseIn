84ee19f9f540885cb33ebc762f92fe8f97d9f77c
Vladislav Krets <vladislav.krets@gmail.com>
2021-04-24 20:32:31 +0300

    security floors

--- CaseIn/views.py
+++ CaseIn/views.py
@@ -7,5 +7,10 @@ from core import models
 
 def my_view(request, pk):
     floor = models.FloorData.objects.get(pk=pk)
+    token = request.COOKIES.get('token', None)
+    if not token:
+        return HttpResponse(status=400)
+    if not Token.objects.filter(key=token).exists():
+        return HttpResponse(status=401)
     response = TemplateResponse(request, 'floor_viewer.html', {'json': mark_safe(floor.json_floor)})
     return response
