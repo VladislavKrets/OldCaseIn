beaad49691e27c154273e6f2a88e237bae84bd87
Vladislav Krets <vladislav.krets@gmail.com>
2021-05-07 19:19:04 +0300

    bot change

--- frontend/src/components/BotContent/BotContent.js
+++ frontend/src/components/BotContent/BotContent.js
@@ -55,54 +55,56 @@ export default class BotContent extends React.Component {
     render() {
         return <div className={'moduleContent-background'}>
             <div className={'moduleContent-no-overflow-parent'}>
-                <div className={'moduleContent'} id={'bot'} style={{height: '85%'}}>
-                    {
-                        this.state.content.length === 0 && <div style={{
-                            width: '100%',
-                            height: '100%',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            textAlign: 'center'
-                        }}>
-                            Здесь вы можете получить ответы на распространенные вопросы.
-                        </div>
-                    }
-                    {
-                        this.state.content.map(item => {
-                            return <div style={{
-                                display: 'flex',
+                <div style={{display: 'flex', flexDirection: 'column', width: '100%', height: '100%'}}>
+                    <div className={'moduleContent'} id={'bot'} style={{flexGrow: 1}}>
+                        {
+                            this.state.content.length === 0 && <div style={{
                                 width: '100%',
-                                boxSizing: 'border-box',
-                                margin: '10px 0',
-                                flexDirection: item.from === 'user' ? 'row-reverse' : null
+                                height: '100%',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                textAlign: 'center'
                             }}>
-                                <div className={'bot-message'}>
-                                    {item.text}
-                                </div>
+                                Здесь вы можете получить ответы на распространенные вопросы.
                             </div>
-                        })
-                    }
-                </div>
-                <hr style={{margin: '10px 0'}}/>
-                <div style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    maxHeight: '15%',
-                    padding: '0 5px',
-                    boxSizing: 'border-box'
-                }}
-                     className={'themes-container'}>
-                    <Form onSubmit={this.onSubmit}>
-                        <div style={{display: 'flex', justifyContent: 'center'}}>
-                            <Form.Control type="text" value={this.state.currentText} onChange={this.handleChange}
-                                          placeholder="Введите вопрос"/>
-                            <div style={{paddingLeft: '5px'}}/>
-                            <Button type="submit">
-                                Отправить
-                            </Button>
-                        </div>
-                    </Form>
+                        }
+                        {
+                            this.state.content.map(item => {
+                                return <div style={{
+                                    display: 'flex',
+                                    width: '100%',
+                                    boxSizing: 'border-box',
+                                    margin: '10px 0',
+                                    flexDirection: item.from === 'user' ? 'row-reverse' : null
+                                }}>
+                                    <div className={'bot-message'}>
+                                        {item.text}
+                                    </div>
+                                </div>
+                            })
+                        }
+                    </div>
+                    <hr style={{margin: '10px 0', position: 'relative', zIndex: 2}}/>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        padding: '12px 5px',
+                        paddingBottom: '40px',
+                        boxSizing: 'border-box'
+                    }}
+                         className={'themes-container'}>
+                        <Form onSubmit={this.onSubmit}>
+                            <div style={{display: 'flex', justifyContent: 'center'}}>
+                                <Form.Control type="text" value={this.state.currentText} onChange={this.handleChange}
+                                              placeholder="Введите вопрос"/>
+                                <div style={{paddingLeft: '5px'}}/>
+                                <Button type="submit">
+                                    Отправить
+                                </Button>
+                            </div>
+                        </Form>
+                    </div>
                 </div>
             </div>
         </div>
