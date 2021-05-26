8519111754bb3f5c1eed1c6012c3269257d155ae
Vladislav Krets <vladislav.krets@gmail.com>
2021-05-26 22:58:12 +0300

    added total graphs

--- frontend/src/components/Master/Master.js
+++ frontend/src/components/Master/Master.js
@@ -142,10 +142,10 @@ class Master extends React.Component {
                 </div>
                 {
                     this.state.graphData.map(item => {
-                        return <div style={{width: '100%', height: '600px', position: 'relative', zIndex: 2}}>
+                        return <div className={'graphic-container'}>
                     <ResponsiveLine
                         data={item.data}
-                        margin={{top: 50, right: 110, bottom: 50, left: 60}}
+                        margin={{top: 50, right: 10, bottom: 50, left: 50}}
                         xScale={{type: 'point'}}
                         yScale={{type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false}}
                         yFormat=" >-.2f"
@@ -176,32 +176,7 @@ class Master extends React.Component {
                         pointBorderColor={{from: 'serieColor'}}
                         pointLabelYOffset={-12}
                         useMesh={true}
-                        legends={[
-                            {
-                                anchor: 'bottom-right',
-                                direction: 'column',
-                                justify: false,
-                                translateX: 100,
-                                translateY: 0,
-                                itemsSpacing: 0,
-                                itemDirection: 'left-to-right',
-                                itemWidth: 80,
-                                itemHeight: 20,
-                                itemOpacity: 0.75,
-                                symbolSize: 12,
-                                symbolShape: 'circle',
-                                symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                                effects: [
-                                    {
-                                        on: 'hover',
-                                        style: {
-                                            itemBackground: 'rgba(0, 0, 0, .03)',
-                                            itemOpacity: 1
-                                        }
-                                    }
-                                ]
-                            }
-                        ]}
+                        motionConfig="wobbly"
                     />
                 </div>
                     })
